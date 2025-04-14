3d93c451435bf9dd2805b4b53490d170ca1a94aa
Pietro Giardina <s233663@dtu.dk>
2025-04-14 19:18:12 +0200

    Reduced stepsize when generating data

--- generate_data.py
+++ generate_data.py
@@ -18,7 +18,7 @@ def find_td(travel_time):
     """
     def inner_find_td(beta, gamma, t_star):
         cost_fun = cost(travel_time)
-        solver = GradientDescent(fun=cost_fun, acceleration=False, stepsize=.1, maxiter=1000)
+        solver = GradientDescent(fun=cost_fun, acceleration=False, maxiter=4000, stepsize=.05)
         lval, _ = solver.run(0., beta, gamma, t_star)
         rval, _ = solver.run(24., beta, gamma, t_star)
         val = jnp.where(cost_fun(rval, beta, gamma, t_star) < cost_fun(lval, beta, gamma, t_star), rval, lval)
