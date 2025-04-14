4c72cad7f816d8f4e6a676c9fca01a4de35e1411
Pietro Giardina <s233663@dtu.dk>
2025-04-14 12:08:38 +0200

    Fixed asymm_gaussian

--- travel_times.py
+++ travel_times.py
@@ -3,7 +3,7 @@ import jax.numpy as jnp
 def asymm_gaussian(sigma_l=.9, sigma_r=.2, mu=9.5):
     left_gau = lambda x: jnp.exp(-(x-mu)**2/sigma_l**2)
     right_gau = lambda x: jnp.exp(-(x-mu)**2/sigma_r**2)
-    return lambda t: jnp.where(t < 0, left_gau(t), right_gau(t))
+    return lambda t: jnp.where(t < mu, left_gau(t), right_gau(t))
 
 def asymm_gaussian_plateau(sigma_l=.9, sigma_r=.2, mu=9.5, plateau_len=3):
     left_gau = lambda x: jnp.exp(-(x-mu)**2/sigma_l**2)
